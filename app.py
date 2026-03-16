d859214df55fb8c9f8f9945c557694f3ceb86167
Nifemi Omoshuli <oluwanifemiomoshuli10@gmail.com>
2026-03-16 15:38:48 +0000

    Add HTML templates and CSS styling for homepage

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Britball Evolution"
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return "<h1>Register page coming soon...</h1>"
+
+@app.route('/login')
+def login():
+    return "<h1>Login page coming soon...</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
